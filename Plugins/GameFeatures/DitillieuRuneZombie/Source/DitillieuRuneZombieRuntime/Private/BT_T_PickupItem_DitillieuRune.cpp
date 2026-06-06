29d0ec1cd09219b3ee2b304787d5791f6603a1cf
Rune Ditillieu <rune.ditillieu@student.howest.be>
2026-06-06 22:51:03 +0200

    Adjusted PickupItem task to prioritize items we have less than 2 of & and to replace items by ones with a better value otherwise

--- Plugins/GameFeatures/DitillieuRuneZombie/Source/DitillieuRuneZombieRuntime/Private/BT_T_PickupItem_DitillieuRune.cpp
+++ Plugins/GameFeatures/DitillieuRuneZombie/Source/DitillieuRuneZombieRuntime/Private/BT_T_PickupItem_DitillieuRune.cpp
@@ -21,6 +21,8 @@ EBTNodeResult::Type UBT_T_PickupItem_DitillieuRune::ExecuteTask(UBehaviorTreeCom
 	GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Green, 
 	FString::Printf(TEXT("Pickup")));
 	
+	//UE_LOG(LogTemp, Warning, TEXT("Start pickup"));
+	
 	// grab survivor
 	AIController = OwnerComponent.GetAIOwner();
 	if (!AIController) return EBTNodeResult::Failed;
@@ -30,7 +32,7 @@ EBTNodeResult::Type UBT_T_PickupItem_DitillieuRune::ExecuteTask(UBehaviorTreeCom
 	
 	// can't pickup if inventory is full
 	InventoryComponent = Survivor->GetComponentByClass<UInventoryComponent>();
-	if (!InventoryComponent->GetInventory().Contains(nullptr)) return EBTNodeResult::Failed;
+	//if (!InventoryComponent->GetInventory().Contains(nullptr)) return EBTNodeResult::Failed;
 	
 	BlackBoard = AIController->GetBlackboardComponent();
 	if (!BlackBoard) return EBTNodeResult::Failed;
@@ -49,18 +51,23 @@ EBTNodeResult::Type UBT_T_PickupItem_DitillieuRune::ExecuteTask(UBehaviorTreeCom
 	// can't pickup if we don't see anything
 	if (VisibleItems.Num() == 0) return EBTNodeResult::Failed;
 	
+	// only pickup items we have 0 or 1 of, 
+	// or that have a better value than our currently owned items of that type
+	ABaseItem* BestItemToPickup = GetBestItemToPickup(VisibleItems);
+	if (BestItemToPickup == nullptr) return EBTNodeResult::Failed;
+	
 	UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld()); //make sure not in wall
 	if (!NavSys) return EBTNodeResult::Failed;
 
 	// grab location of first visible item
 	FNavLocation ProjectedLocation;
-	bool bFoundValidSpot = NavSys->ProjectPointToNavigation(VisibleItems[0]->GetActorLocation(), ProjectedLocation, FVector(200.f, 200.f, 200.f));
+	bool bFoundValidSpot = NavSys->ProjectPointToNavigation(BestItemToPickup->GetActorLocation(), ProjectedLocation, FVector(200.f, 200.f, 200.f));
 	Survivor->StopRunning();
 
 	// if valid, set item to pickup in the coming frames
 	if (bFoundValidSpot)
 	{
-		ItemToPickup = VisibleItems[0];
+		ItemToPickup = BestItemToPickup;
 		ItemLocation = ProjectedLocation;
 		NumItems = VisibleItems.Num();
 		return EBTNodeResult::InProgress;
@@ -71,6 +78,7 @@ EBTNodeResult::Type UBT_T_PickupItem_DitillieuRune::ExecuteTask(UBehaviorTreeCom
 
 void UBT_T_PickupItem_DitillieuRune::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
+	//UE_LOG(LogTemp, Warning, TEXT("Picking up"));
 	// move towards item
 	EPathFollowingRequestResult::Type MoveResult = AIController->MoveToLocation(ItemLocation.Location, 
 		25.0f, false, true, true, true, 0, true);
@@ -84,6 +92,8 @@ void UBT_T_PickupItem_DitillieuRune::TickTask(UBehaviorTreeComponent& OwnerComp,
 	if (MoveResult == EPathFollowingRequestResult::AlreadyAtGoal
 		|| (Survivor->GetActorLocation() - ItemLocation.Location).Length() < 50.f)
 	{
+		RemoveTripledItems();
+		
 		int Idx{ 0 };
 		for (auto Item : InventoryComponent->GetInventory())
 		{
@@ -122,12 +132,113 @@ void UBT_T_PickupItem_DitillieuRune::TickTask(UBehaviorTreeComponent& OwnerComp,
 						BlackBoard->SetValueAsBool(FName("SensedSomething"), false);
 					}
 				}
-					
+				
 				InventoryComponent->GrabItem(Idx, ItemToPickup);
 			}
 			++Idx;
 		}
 			
 		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
+
+ABaseItem* UBT_T_PickupItem_DitillieuRune::GetBestItemToPickup(TArray<ABaseItem*> KnownItems)
+{
+	ABaseItem* BestItem{ nullptr };
+	TArray<int> AmItemsPerType{};
+	
+	int Idx{ -1 };
+	for (auto* Item : KnownItems)
+	{
+		++Idx;
+		
+		AmItemsPerType.Add(0);
+		for (auto* OwnedItem : InventoryComponent->GetInventory())
+		{
+			if (OwnedItem == nullptr) continue;
+			
+			if (OwnedItem->GetItemType() == Item->GetItemType())
+			{
+				AmItemsPerType[Idx] += 1;
+			}
+		}
+	}
+	
+	Idx = -1;
+	for (auto* Item : KnownItems)
+	{
+		++Idx;
+		if (AmItemsPerType[Idx] < 2)
+		{
+			BestItem = Item;
+			return BestItem;
+		}
+	}
+	
+	Idx = -1;
+	for (auto* Item : KnownItems)
+	{
+		++Idx;
+		bool HasBetterValue{ false };
+		for (auto* OwnedItem : InventoryComponent->GetInventory())
+		{
+			if (OwnedItem == nullptr) continue;
+			
+			if (OwnedItem->GetItemType() == Item->GetItemType() 
+				&& Item->GetValue() > OwnedItem->GetValue())
+			{
+				HasBetterValue = true;
+			}
+		}
+		
+		if (HasBetterValue)
+		{
+			BestItem = Item;
+			return Item;
+		}
+	}
+	
+	return BestItem;
+}
+
+void UBT_T_PickupItem_DitillieuRune::RemoveTripledItems()
+{
+	TArray<int> ItemsOfThisType{};
+	
+	// check how many items of this type we already had
+	int Idx{ -1 };
+	//UE_LOG(LogTemp, Warning, TEXT("Old Inventory: "));
+	for (auto* OwnedItem : InventoryComponent->GetInventory())
+	{
+		++Idx;
+		if (OwnedItem == nullptr) continue;
+			
+		//UE_LOG(LogTemp, Warning, TEXT("Slot %d : %d"), Idx, OwnedItem->GetValue());
+		
+		if (OwnedItem->GetItemType() == ItemToPickup->GetItemType())
+		{
+			ItemsOfThisType.Add(Idx);
+		}
+	}
+	
+	// if we already had more than 1
+	// remove the one with the lowest value if that one is lower than the new one
+	if (ItemsOfThisType.Num() > 1)
+	{
+		int LowestValueItem{ -1 };
+		int LowestValue{ 10 };
+		for (auto ItemIdx : ItemsOfThisType)
+		{
+			if (InventoryComponent->GetInventory()[ItemIdx]->GetValue() < LowestValue)
+			{
+				LowestValue = InventoryComponent->GetInventory()[ItemIdx]->GetValue();
+				LowestValueItem = ItemIdx;
+			}
+		}
+		
+		if (LowestValueItem > -1 && LowestValue < ItemToPickup->GetValue())
+		{
+			InventoryComponent->RemoveItem(LowestValueItem);
+		}
 	}
 }
--- Plugins/GameFeatures/DitillieuRuneZombie/Source/DitillieuRuneZombieRuntime/Public/BT_T_PickupItem_DitillieuRune.h
+++ Plugins/GameFeatures/DitillieuRuneZombie/Source/DitillieuRuneZombieRuntime/Public/BT_T_PickupItem_DitillieuRune.h
@@ -21,6 +21,8 @@ public:
 	UBT_T_PickupItem_DitillieuRune();
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComponent, uint8* TaskMemory) override;
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	ABaseItem* GetBestItemToPickup(TArray<ABaseItem*> KnownItems);
+	void RemoveTripledItems();
 	
 private:
 	ABaseItem* ItemToPickup{ nullptr };
